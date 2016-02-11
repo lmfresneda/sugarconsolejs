80dc7a687615598c021f4dd45f83982c07d6bed9
lmfresneda <lmfresneda@bitbucket.org>
2016-02-11 18:10:02 +0100

    add rest of project

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+const rename = require("gulp-rename");
+const uglify = require("gulp-uglify");
+ 
+gulp.task('babelify', function() {
+	console.log("******* Babelizando 'src/sugar.console.es2015.js'");
+	return gulp.src('src/sugar.console.es2015.js')
+		.pipe(babel())
+		.pipe(rename("sugar.console.js"))
+		.pipe(gulp.dest('dist'));
+});
+
+gulp.task('uglify', function() {
+	console.log("******* Comprimiendo y babelizando 'src/sugar.console.es2015.js'");
+	return gulp.src('src/sugar.console.es2015.js')
+		.pipe(babel())
+		.pipe(uglify())
+		.pipe(rename("sugar.console.min.js"))
+		.pipe(gulp.dest('dist'));
+});
+
+
+gulp.task("default", function() {
+	console.log("******* Escuchando cambios en 'src/sugar.console.es2015.js'");
+	gulp.watch('src/sugar.console.es2015.js', ['babelify', "uglify"]);
+});
